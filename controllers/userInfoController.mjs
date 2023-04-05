80cf52e6f411e93290009870f7b2fcad367cd0de
gret.za <greta.zaveckaite@gmail.com>
2023-04-05 19:05:11 +0300

    Final

--- controllers/userInfoController.mjs
+++ controllers/userInfoController.mjs
@@ -3,30 +3,17 @@ import { decodeToken } from "../utils/tokens.mjs";
 
 export const userInfoController = (req, res) => {
   console.log(usersList);
+
+  if (!req.user) {
+    res.status(403).json({ message: "User not logged in." });
+    return;
+  }
   const authId = decodeToken(req.headers.token)?._id;
   const user = usersList.find(({_id}) => _id === authId)
-  res.json();
 
+  res.json(user);
+};
 
-  // res.json({
-  //   email: user.email,
-  //   firstName: user.firstName,
-  //   lastName: user.lastName,
-  //   address: user.address,
-  //   gender: user.gender,
-  //   newsletter: user.newsletter,
-  // })
-
-
-  // if (!req.user) {
-  //   res.status(403).json({ message: "User not logged in." });
-  //   return;
-  // }
-
-  // if (!usersList[req.user._id]) {
-  //   usersList[req.user._id] = [];
-  // }
-  // const userInfo = usersList[req.user._id];
 
  
-};
+
